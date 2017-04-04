7d520d59fa903b1a3e1ab69bf4b47359f5e45d9c
yakami <c360785655@gmail.com>
2017-04-05 01:54:02 +0800

    init req

--- src/component/fragment/AmazonRank.js
+++ src/component/fragment/AmazonRank.js
@@ -1,8 +1,9 @@
 // 主页
-import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity, Image, Button} from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Image, Button } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
-import {TabViewAnimated, TabBar} from 'react-native-tab-view'
+import { TabViewAnimated, TabBar } from 'react-native-tab-view'
+import JSONReqService from '../../service/request/JSONReqService'
 
 export default class AmazonRank extends Component {
 
@@ -10,10 +11,10 @@ export default class AmazonRank extends Component {
         title: '日亚排名',
         tabBar: {
             label: '日亚排名',
-            icon: ({tintColor}) => (
+            icon: ({ tintColor }) => (
                 <Image
                     source={require('../../assets/icon/trending_black.png')}
-                    style={[styles.icon, {tintColor: tintColor}]}
+                    style={[styles.icon, { tintColor: tintColor }]}
                 />
             )
         },
@@ -22,48 +23,49 @@ export default class AmazonRank extends Component {
             //     source={require('../../assets/icon/more_white.png')}
             //     style={{width: 26, height: 26, marginRight: 5}}
             // />,
-            style: {backgroundColor: '#3498db'},
-            titleStyle: {color: '#ffffff'},
+            style: { backgroundColor: '#3498db' },
+            titleStyle: { color: '#ffffff' },
         }
     };
 
     state = {
         index: 0,
+        dataList: null,
         routes: [
-            {key: '1', title: '实时TOP100'},
-            {key: '2', title: '4月新番'},
-            {key: '3', title: '1月新番'},
-            {key: '4', title: '1月新番'},
-            {key: '5', title: '1月新番'},
+            { key: '1', title: '实时TOP100' },
+            { key: '2', title: '4月新番' },
+            { key: '3', title: '1月新番' },
+            { key: '4', title: '1月新番' },
+            { key: '5', title: '1月新番' },
         ],
     };
 
     _handleChangeTab = (index) => {
-        this.setState({index});
+        this.setState({ index });
     };
 
     _renderHeader = (props) => {
         return <TabBar
             {...props}
-            tabStyle={{height: 42}}
+            tabStyle={{ height: 42 }}
             pressOpacity={1}
             pressColor='#3498db'
-            style={{backgroundColor: '#fff'}}
-            indicatorStyle={{backgroundColor: '#3498db', zIndex: 1000, height: 2, opacity: 1}}
+            style={{ backgroundColor: '#fff' }}
+            indicatorStyle={{ backgroundColor: '#3498db', zIndex: 1000, height: 2, opacity: 1 }}
             scrollEnabled={true}
             // tabWidth="50"
-            labelStyle={{color: 'rgba(0,0,0,0.6)'}}
+            labelStyle={{ color: 'rgba(0,0,0,0.6)' }}
         />;
     };
 
-    _renderScene = ({route}) => {
+    _renderScene = ({ route }) => {
         switch (route.key) {
             case '1':
-                return <View style={[styles.page, {backgroundColor: '#1abc9c'}]}/>;
-            case '2':
-                return <View style={[styles.page, {backgroundColor: '#2ecc71'}]}/>;
-            case '3':
-                return <View style={[styles.page, {backgroundColor: '#9b59b6'}]}/>;
+                return <View style={[styles.page, { backgroundColor: '#1abc9c' }]} />;
+            case '18':
+                return <View style={[styles.page, { backgroundColor: '#2ecc71' }]} />;
+            case '20':
+                return <View style={[styles.page, { backgroundColor: '#9b59b6' }]} />;
             default:
                 return null;
         }
@@ -71,18 +73,38 @@ export default class AmazonRank extends Component {
 
     componentDidMount() {
         setTimeout(() => SplashScreen.hide(), 500);
+        new JSONReqService('http://anime-discs.com/sakura.do')
+            .request()
+            .then(res => {
+                console.log(res);
+                const routes = res.map(item => {
+                    return {
+                        key: item.id.toString(),
+                        title: item.title
+                    }
+                })
+                this.setState({dataList: res, routes});
+            })
     }
 
     render() {
-        return (
-            <TabViewAnimated
-                style={styles.container}
-                navigationState={this.state}
-                renderScene={this._renderScene}
-                renderHeader={this._renderHeader}
-                onRequestChangeTab={this._handleChangeTab}
-            />
-        );
+        if (this.state.dataList) {
+            return (
+                <TabViewAnimated
+                    style={styles.container}
+                    navigationState={this.state}
+                    renderScene={this._renderScene}
+                    renderHeader={this._renderHeader}
+                    onRequestChangeTab={this._handleChangeTab}
+                />
+            );
+        } else {
+            return (
+                <View>
+                    <Text>Loading...</Text>
+                </View>
+            )
+        }
     }
 
 }
---
+++ src/service/request/JSONReqService.js
@@ -0,0 +1,72 @@
+class JSONReqService {
+    constructor(url) {
+        this.url = url;
+        this.method = 'GET';
+        this.credentials = 'include';
+        this.retryTimes = 5;
+        this.curRetryTimes = -1;
+        this.retryInterval = 1; // secs
+        this.enabledLog = true;
+        this.fetchSetting = null;
+    }
+
+    setMethod(method) {
+        this.method = method;
+        return this;
+    }
+
+    setCredentials(credential) {
+        this.credentials = credential;
+        return this;
+    }
+
+    setFetchSetting(setting) {
+        this.fetchSetting = setting;
+        return this;
+    }
+
+    setRetryTimes(times) {
+        this.retryTimes = times;
+    }
+
+    setRetryInterval(secs) {
+        this.retryInterval = secs;
+    }
+
+    request() {
+        return new Promise((resolve, reject) => {
+            this._request(res => {
+                res.text().then(text => resolve(JSON.parse(text)));
+            }, err => {
+                reject(err);
+            });
+        });
+    }
+
+    _printErrorLog(err) {
+        console.log(`JSONReqService: request error in ${this.url}, retry:(${this.curRetryTimes}/${this.retryTimes}), error: ${err}`);
+    }
+
+    _request(successCallback, failureCallback) {
+        this.curRetryTimes++;
+        let url = this.url.includes('http') ? this.url : `${window.location.protocol}//${window.location.host}${this.url}`;
+        window.fetch(url, this.fetchSetting ? this.fetchSetting : {
+            method: this.method,
+            credentials: this.credentials
+        }).then(res => {
+            successCallback(res);
+        }, err => {
+            console.log(err);
+            this._printErrorLog(err);
+            if (this.curRetryTimes < this.retryTimes) {
+                setTimeout(() => {
+                    this._request(successCallback, failureCallback);
+                }, this.retryInterval * 1000);
+            } else {
+                failureCallback(err);
+            }
+        });
+    }
+}
+
+export default JSONReqService;
